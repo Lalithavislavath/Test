a627a99f5b868de01530b108cc6e0727a5c73492
BadeMadhavi <bademadhavi74586@gmail.com>
2024-12-05 09:14:14 +0530

    2nd modified code

--- new/src/Components/Name.jsx
+++ new/src/Components/Name.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 
 function Name() {
     const [name,SetName]=useState
+     const handleclick =()=>{
+     }
   return (
     <div>
        <input type='text' value={name} onChange={(e)=>SetName(e.target.value)}/>
+       <button onClick={handleclick}>click me</button>
     </div>
   )
 }
